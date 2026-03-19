580e68d59805bc9b1300292834abb2c8b0a266d6
monaski <skidev101@gmail.com>
2026-03-19 17:19:33 +0100

    create vendor middleware to verify role

--- src/middlewares/authorize.middleware.ts
+++ src/middlewares/authorize.middleware.ts
@@ -1,12 +1,12 @@
-import { NextFunction, Response } from "express";
-import { AuthRequest } from "./auth.middleware";
-import { ForbiddenError } from "../utils/api-errors";
+  import { NextFunction, Response } from "express";
+  import { AuthRequest } from "./auth.middleware";
+  import { ForbiddenError } from "../utils/api-errors";
 
-export const authorize = (...allowedRoles: string[]) => {
-  return (req: AuthRequest, _res: Response, next: NextFunction) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      throw new ForbiddenError("You do not have permission to access this.");
-    }
-    next();
+  export const authorize = (...allowedRoles: string[]) => {
+    return (req: AuthRequest, _res: Response, next: NextFunction) => {
+      if (!req.user || !allowedRoles.includes(req.user.role)) {
+        throw new ForbiddenError("You do not have permission to access this.");
+      }
+      next();
+    };
   };
-};
